e7b11962fbca68c280d4428706d4165105da2324
Zach Stybel <zstybel@gmail.com>
2026-09-10 16:32:30 -0400

    Keep updated personal interests wording

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero({ onComplete }) {
           <p>I'm a senior studying Computer Science at CUNY Baruch College, graduating in 2027. I just wrapped up my internship at Bloomberg where I worked on Bloomberg’s award-winning evaluated pricing service, BVAL.</p>
           <p>I mainly worked with Python and gained hands-on experience with Docker for local development, unit/integration testing, designing distributed systems, and database/schema design.</p>
           <p>I enjoy participating in hackathons and won prizes at Columbia and City College. Those events taught me how to turn ambitious ideas into useful products under pressure and work collaboratively.</p>
-          <p>Outside of work and school, you I usually play Rainbow Six Siege, watch football, go to the gym, or cheffing up in the kitchen.</p>
+          <p>Outside of work and school, I usually play Rainbow Six Siege, watch football (both of them), go to the gym, or chef it up in the kitchen.</p>
           <p>This portfolio is a record of the projects, tools, and experiences that have shaped the engineer I'm becoming and I hope Disney does not sue me.</p>
         </div>
       </div>
